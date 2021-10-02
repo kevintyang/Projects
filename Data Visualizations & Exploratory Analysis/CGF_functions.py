7966bb8d135207d4b96a8342f26b02e456b1f15a
Kevguin <79610249+Kevguin@users.noreply.github.com>
2021-10-02 13:38:54 -0600

    update

--- Data Visualizations & Exploratory Analysis/CGF_functions.py
+++
@@ -1,6 +0,0 @@
-def col_stats(list,column):
-    #input list of dataframes for analysis
-    #input column for needed stats, as string
-
-    for i,table in enumerate(list):
-        table[column].describe().T
